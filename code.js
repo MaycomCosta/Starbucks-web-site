a100f38f7b698c0a489184107f3641e7564525bb
MaycomCosta <maycom19costa@gmail.com>
2021-07-16 14:02:45 -0300

    Update

---
+++ code.js
@@ -0,0 +1,8 @@
+slider = (anything) => {
+    document.querySelector('.starbucks').src = anything
+}
+
+changeColor = (color) => {
+    const circle = document.querySelector('.circle')
+    circle.style.background = color;
+}
